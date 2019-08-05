a4b66505923b68949672e92e96dd23c5ba6425c3
shaynelyj <47095119+shaynelyj@users.noreply.github.com>
2019-08-05 22:14:55 +0800

    Create print text.asm

---
+++ print text.asm
@@ -0,0 +1,33 @@
+;Program auto calculate length of text to print text directly
+
+section .data
+	text db "Hello World",10,0
+
+section .text
+	global _start
+
+_start:
+	mov rax, text
+	call _print
+
+	mov rax, 60
+	mov rdi, 0
+	syscall
+
+_print:
+	push rax
+	mov rbx, 0
+
+_printLoop:
+	inc rax
+	inc rbx
+	mov cl, [rax]
+	cmp cl, 0
+	jne _printLoop
+
+	mov rax, 1
+	mov rdi, 1
+	pop rsi
+	mov rdx, rbx
+	syscall
+	ret
